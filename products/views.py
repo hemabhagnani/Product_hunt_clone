4a5ddd157e64af04136ef1a16de3d3d2caae1a90
hemabhagnani <hemabhagnani5@gmail.com>
2019-10-20 20:37:58 +0530

    product details

--- products/views.py
+++ products/views.py
@@ -1,4 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Product
+from django.utils import timezone
 def home(request):
-    return render(request,'products/home.html')
+    products=Product.objects
+    return render(request,'products/home.html',{'products':products})
 # Create your views here.
+@login_required(login_url="/accounts/signup")
+def create(request):
+    if request.method=="POST":
+        if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['image'] and request.FILES['icon']:
+            product=Product()
+            product.title=request.POST['title']
+            product.body=request.POST['body']
+            if request.POST['url'].startswith('http://')or request.POST['url'].startswith('https://'):
+                product.url=request.POST['url']
+            else:
+                product.url='http://'+request.POST['url']
+
+            product.Icon=request.FILES['icon']
+            product.Image=request.FILES['image']
+            product.pub_date=timezone.datetime.now()
+            product.hunter=request.user
+            product.save()
+            return redirect('/products/'+ str(product.id))
+    else:
+        return render(request,'products/create.html',{'error':'all fields are required'})
+
+
+def detail(request,product_id):
+        product=get_object_or_404(Product,pk=product_id)
+        return render(request,'products/detail.html',{'product':product})
+
+@login_required(login_url="/accounts/signup")
+def upvote(request,product_id):
+    if request.method == "POST":
+        product=get_object_or_404(Product,pk=product_id)
+        product.votes_total += 1
+        product.save()
+        return redirect('/products/'+ str(product.id))
